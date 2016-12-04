ad36a6259faef1ac64dfa293c66a0b206d7157e8
melody5541 <goooooooooodboy@icloud.com>
2016-12-05 05:31:42 +0800

    test1

--- src/main/java/com/javarticles/spring/integration/jdbc/SpringIntegrationJdbcInboundRowMapperExample.java
+++ src/main/java/com/javarticles/spring/integration/jdbc/SpringIntegrationJdbcInboundRowMapperExample.java
@@ -4,12 +4,18 @@ import java.io.IOException;
 import java.sql.SQLException;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.integration.channel.DirectChannel;
+import org.springframework.messaging.PollableChannel;
+import org.springframework.messaging.support.GenericMessage;
 
 public class SpringIntegrationJdbcInboundRowMapperExample {
 
     public static void main(String[] args) throws InterruptedException, IOException, SQLException {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
                 "jdbcInboundApplicationContext.xml");
+        PollableChannel inputChannel = context.getBean("dataChannel1", PollableChannel.class);
+		inputChannel.send(new GenericMessage<String>("World"));
+		inputChannel.receive().getPayload();
         Thread.sleep(1000);
         context.close();
     }
