6470ef55ea4d8353f42d608b327841bb0052e3be
melody5541 <goooooooooodboy@icloud.com>
2017-01-22 02:22:52 +0800

    。。。

--- src/main/java/com/hsbc/srbp/commonMsg/domain/JdbcMessageHandler.java
+++ src/main/java/com/hsbc/srbp/commonMsg/domain/JdbcMessageHandler.java
@@ -1,6 +1,6 @@
 package com.hsbc.srbp.commonMsg.domain;
 
-import java.util.ArrayList;
+import java.sql.SQLException;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -11,21 +11,18 @@ public class JdbcMessageHandler {
 	private static Logger logger = Logger.getLogger(JdbcMessageHandler.class);
 
 	private AtomicInteger counter = new AtomicInteger();
-
+	
+	
 	public void handleMessage(Message<?> message) throws Exception {
-		if (message.getPayload() instanceof ArrayList<?>) {
 			int count = counter.incrementAndGet();
 			List<CommonMessage> list = (List<CommonMessage>) message.getPayload();
-			logger.info("Current Thread:" + Thread.currentThread().getName() + " sleeping in #"+count+" poll");
-			Thread.sleep(5000);
-			logger.info("Current Thread:" + Thread.currentThread().getName() + " waking up in #"+count+" poll");
 			for (CommonMessage msg : list) {
-				logger.info(Thread.currentThread().getName() + " prepared message #" + count + " for message #"
-						+ msg.getMessage_id() + ": " + msg);
+				if(msg.getMessage_name().equals("name1")){
+					logger.info("Current Thread:" + Thread.currentThread().getName() + " simulater throws exception and sleeping with "+count+" poll");
+					Thread.sleep(10000);
+					throw new SQLException("simulater throws exception");
+				}
+				logger.info(Thread.currentThread().getName() + " handle message #" + msg.getMessage_id() + ": " + msg);
 			}
-		} else {
-			throw new Exception("message not a instance of CommonMessage");
-		}
-
 	}
 }
--- src/main/java/com/hsbc/srbp/commonMsg/test/SiJdbcInboundAutoOneByOne.java
+++ src/main/java/com/hsbc/srbp/commonMsg/test/SiJdbcInboundAutoOneByOne.java
@@ -4,12 +4,19 @@ import java.io.IOException;
 import java.sql.SQLException;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.PollableChannel;
+import org.springframework.messaging.SubscribableChannel;
 
 public class SiJdbcInboundAutoOneByOne {
 
     public static void main(String[] args) throws InterruptedException, IOException, SQLException {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(
                 "jdbcInboundApplicationContext.xml");
+        MessageChannel channel = context.getBean("msgQueryOutputChannel",MessageChannel.class);
+        if(channel instanceof SubscribableChannel){
+        	System.out.println(true);
+        }
     }
     
 }
